a7cfd619952351f28f3c42d13dfb417bdb0f9aef
aakashraj001 <aakstheanimator.25@gmail.com>
2024-09-13 00:59:58 +0530

    added

---
+++ src/pages/components/navbar/navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+
+<nav className="navbar">
+    <p>logo</p>
+    
+</nav>
+);
+};
+
+export default Navbar;
